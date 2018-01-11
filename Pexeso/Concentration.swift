da48181b0e514cd705f17944051dcf3f58a900ee
Artem Reznikov <temarez@gmail.com>
2018-01-11 13:31:20 +0200

    New class "Concentration" inspired by Stanford CS 193P courses

---
+++ Pexeso/Concentration.swift
@@ -0,0 +1,44 @@
+//
+//  Concentration.swift
+//  Pexeso
+//
+//  Created by Artem Rieznikov on 09.01.18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]() // Array<Card>
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card] //Same as twice calling cards.append(card)
+        }
+        // TODO: Shuffle the cards
+    }
+}
