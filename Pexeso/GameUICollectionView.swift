0b93f9aa910ef6821b1492496b6cee9aa218d230
Artem Reznikov <temarez@gmail.com>
2018-01-17 13:09:38 +0200

    Replace CollectionViewController with ViewController containing CollectionView (part 1)

---
+++ Pexeso/GameUICollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  GameUICollectionView.swift
+//  Pexeso
+//
+//  Created by Artem Rieznikov on 17.01.18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import UIKit
+
+class GameUICollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
