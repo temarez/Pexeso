c33201b9372ac49ecffb47d105767124bbe0b1fb
Artem Reznikov <temarez@gmail.com>
2018-01-22 20:39:25 +0200

    Added Score class

---
+++ Pexeso/Score.swift
@@ -0,0 +1,19 @@
+//
+//  Score.swift
+//  Pexeso
+//
+//  Created by Artem Rieznikov on 22.01.18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import Foundation
+
+class Score {
+    var nickName: String = ""
+    var cardsPairs: Int = 1
+    var timeInSec: Int = 1
+    var flips: Int = 2
+    var score: Int {
+        return 100; // TODO: 
+    }
+}
