cea493f47f76540f2434f64f629fa94b80244db8
Artem Reznikov <temarez@gmail.com>
2018-01-25 15:46:40 +0200

    Added HighScores ViewController

---
+++ Pexeso/HighScoresVC.swift
@@ -0,0 +1,35 @@
+//
+//  HighScoresVC.swift
+//  Pexeso
+//
+//  Created by Artem Rieznikov on 25.01.18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import UIKit
+
+class HighScoresVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
