0ebee4011243d001216cab543a066fe88613966b
Artem Reznikov <temarez@gmail.com>
2018-01-29 23:11:19 +0200

    Improve Pexeso class code (Concentration.swift)

--- Pexeso/GameScreen/Controller/Concentration.swift
+++ Pexeso/GameScreen/Controller/Concentration.swift
@@ -9,9 +9,47 @@
 import Foundation
 
 class Pexeso {
-    var cards = [Card]() // Array<Card>
+    private(set) var cards = [Card]() // Array<Card>
+    
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)) : Choosen index not in the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                // either no cards or 2 cards are face up
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
     
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards")
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card] //Same as twice calling cards.append(card)
