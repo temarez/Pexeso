5a4759951cb3e9ee3001d774ccd57ccde258d99d
Artem Reznikov <temarez@gmail.com>
2018-01-29 23:13:03 +0200

    Remove Concentration class and rename Concentration.swift to Pexeso.swift

--- Pexeso/GameScreen/Controller/Pexeso.swift
+++ Pexeso/GameScreen/Controller/Pexeso.swift
@@ -1,5 +1,5 @@
 //
-//  Concentration.swift
+//  Pexeso.swift
 //  Pexeso
 //
 //  Created by Artem Rieznikov on 09.01.18.
@@ -73,38 +73,3 @@ extension MutableCollection {
     }
 }
 
-// TODO: delete below part later
-class Concentration
-{
-    var cards = [Card]() // Array<Card>
-    
-    var indexOfOneAndOnlyFaceUpCard: Int?
-    
-    func chooseCard(at index: Int) {
-        if !cards[index].isMatched {
-            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                if cards[matchIndex].identifier == cards[index].identifier {
-                    cards[matchIndex].isMatched = true
-                    cards[index].isMatched = true
-                }
-                cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
-            } else {
-                // either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = index
-            }
-        }
-    }
-    
-    init(numberOfPairsOfCards: Int) {
-        for _ in 1...numberOfPairsOfCards {
-            let card = Card()
-            cards += [card, card] //Same as twice calling cards.append(card)
-        }
-        // TODO: Shuffle the cards
-    }
-}
