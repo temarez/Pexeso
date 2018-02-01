05c4d08a4b56d40610c36526354f6d4a6144bef9
Artem Reznikov <temarez@gmail.com>
2018-02-01 11:34:53 +0200

    Move Difficulty to DifficultyVC

--- Pexeso/StartScreen/DifficultyVC.swift
+++ Pexeso/StartScreen/DifficultyVC.swift
@@ -8,6 +8,29 @@
 
 import UIKit
 
+struct Difficulty {
+    var numberOfCardPairs: Int = 1
+    
+    init(_ numberOfCardPairs: Int) {
+        self.numberOfCardPairs = numberOfCardPairs
+    }
+    
+    func getRowsCollomnsNumber() -> (shortSide: Int, longSide: Int) {
+        switch numberOfCardPairs {
+        case 1: return (shortSide: 1, longSide: 2)
+        case 4: return (shortSide: 2, longSide: 4)
+        case 6: return (shortSide: 3, longSide: 4)
+        case 8: return (shortSide: 4, longSide: 4)
+        case 10: return (shortSide: 4, longSide: 5)
+        case 12: return (shortSide: 4, longSide: 6)
+        default:
+            let short = Int(Float(numberOfCardPairs*2).squareRoot())
+            let long = Int((numberOfCardPairs*2) / short) + 1
+            return (shortSide: short, longSide: long)
+        }
+    }
+}
+
 protocol NumOfPairsPickerDelegate {
     func getPickerViewSelectedNumOfPairs() -> Int
 }
