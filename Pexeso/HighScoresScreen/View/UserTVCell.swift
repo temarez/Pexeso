bce2b8e71e686886692d47a80ea8dc982c7ebbf3
Artem Reznikov <temarez@gmail.com>
2018-02-01 20:01:17 +0200

    Add scoreLabel to user cell

--- Pexeso/HighScoresScreen/View/UserTVCell.swift
+++ Pexeso/HighScoresScreen/View/UserTVCell.swift
@@ -12,5 +12,6 @@ class UserTVCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
 }
