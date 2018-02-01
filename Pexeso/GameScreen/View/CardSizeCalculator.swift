992e76a70d026e9b04b48b8fcad8c06a2145b830
Artem Reznikov <temarez@gmail.com>
2018-02-01 13:06:14 +0200

    Improve getCardSize()

--- Pexeso/GameScreen/View/CardSizeCalculator.swift
+++ Pexeso/GameScreen/View/CardSizeCalculator.swift
@@ -65,7 +65,8 @@ extension CGSize {
 
 struct CardSizeConstants {
     static var cardSizeMax = CGSize(width: 1024, height: 1024)
-    static var spacingBetweenCards = 10
+    static var minSpacingBetweenCards = 10 // TODO: implement
+    static var aspectRatio: Double = 1.0 // TODO: implement
 }
 
 class CardSizeCalculator {
@@ -73,28 +74,22 @@ class CardSizeCalculator {
     
     public func getCardSize(collectionViewFrameSize: CGSize, numberOfPairs: Int) -> CGSize {
         let numberOfCards = numberOfPairs * 2
-        let shortSideSize = collectionViewFrameSize.shortSideSize()
-        //print("ShortSideSize: \(shortSideSize)")
-        var calculatedCardWidth = CGFloat(1)
-        var calculatedCardHeight = CGFloat(1)
+        let collectionViewShortSideSize = collectionViewFrameSize.shortSideSize()
+        let collectionViewLongSideSize = collectionViewFrameSize.longSideSize()
+
+        var cardShortSideSize = CGFloat(1)
+        var cardLongSideSize = CGFloat(1)
         
         let rowsCollomnsNumber = Difficulty(numberOfPairs).getRowsCollomnsNumber()
-        print("Fitting \(numberOfCards) cards to collection, it will be \(rowsCollomnsNumber.shortSide)x\(rowsCollomnsNumber.longSide)")
         
-        if numberOfCards <= 12 {
-            calculatedCardWidth = shortSideSize/3-16
-            calculatedCardHeight = shortSideSize/3-16
-        }
-        else if numberOfCards <= 24 {
-            calculatedCardWidth = shortSideSize/4-14
-            calculatedCardHeight = shortSideSize/4-14
-        }
-        else {
-            calculatedCardWidth = shortSideSize/5-12
-            calculatedCardHeight = shortSideSize/5-12
-        }
-
-        let calculatedCardSize = CGSize(width: calculatedCardWidth, height: calculatedCardHeight)
+        // Calculate the maximal possible card size that will fit (without aspect ratio and spacings between cards)
+        cardShortSideSize = collectionViewShortSideSize / CGFloat(rowsCollomnsNumber.shortSide)
+        cardLongSideSize = collectionViewLongSideSize / CGFloat(rowsCollomnsNumber.longSide)
+        
+        cardShortSideSize = cardShortSideSize - 20
+        cardLongSideSize = cardLongSideSize - 20
+        
+        let calculatedCardSize = CGSize(width: cardShortSideSize, height: cardLongSideSize)
         /*
         if(collectionViewFrameSize.width == 716.0) { // this is for landscape orientation
             calculatedCardSize = CGSize(width: 100, height: 100)
